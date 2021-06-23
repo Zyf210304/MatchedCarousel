579a8e3fbbc40ce228dc9153e06302d37943d686
张亚飞 <672233580@qq.com>
2021-06-23 09:56:30 +0800

    Initial Commit

---
+++ MatchedCarousel/MatchedCarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchedCarouselApp.swift
+//  MatchedCarousel
+//
+//  Created by 张亚飞 on 2021/6/23.
+//
+
+import SwiftUI
+
+@main
+struct MatchedCarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
